2afd0151ba67ef3cba1ed98949385cbb12f7dc0a
Charan Kumar N <64530924+charan-kumar-137@users.noreply.github.com>
2023-03-11 21:52:25 +0530

    Added Places Endpoint (#1)

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-const express = require("express");
-const app = express();
- 
-//middleware
-app.use(express.json());
- 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
- 
-module.exports = app;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const mongoose = require("mongoose");
-//configure mongoose
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to MongoDB");
-    }
-  });
+require("dotenv").config()
+
+// Views
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var placesRouter = require("./routes/places")
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Views
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/places", placesRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
